100a696fc81a9f2fb314f3d4895286a5b108396f
Lucas Galvao <lucas.engcomp@outlook.com>
2022-10-30 11:20:21 -0300

    :sparkles: adicionado testes simples do body

---
+++ rest-assured/src/test/java/rest/BodyTest.java
@@ -0,0 +1,52 @@
+package rest;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class BodyTest {
+
+    @Test
+    public void devoValidarRespostaBody() {
+        given()
+                .when()
+                .get("http://restapi.wcaquino.me/ola")
+                .then()
+                .statusCode(200)
+                .body(Matchers.is("Ola Mundo!"));
+    }
+
+    @Test
+    public void devoValidarSeBodyContemStringOlaMundo() {
+        given()
+        .when()
+            .get("http://restapi.wcaquino.me/ola")
+        .then()
+            .statusCode(200)
+            .body(Matchers.is("Ola Mundo!"));
+    }
+
+    @Test
+    public void devoValidarSeBodyNaoContemNulo() {
+        given()
+        .when()
+            .get("http://restapi.wcaquino.me/ola")
+        .then()
+            .statusCode(200)
+            .body(is(not(nullValue())));
+    }
+
+    @Test
+    public void devoValidarTodoBody() {
+        given()
+        .when()
+            .get("http://restapi.wcaquino.me/ola")
+        .then()
+            .statusCode(200)
+            .body(Matchers.is("Ola Mundo!"))
+            .body(containsString("Mundo"))
+            .body(is(not(nullValue())));
+    }
+}
